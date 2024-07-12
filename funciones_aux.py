479e3bd0194840d9cf5db008c106619193720f4f
Kevin Gonzalez <kejogo.dev@outlook.com>
2024-07-12 16:24:25 -0500

    feat: :sparkles: mostrar ciudades en terminal paginadamente y flujo en main
    kevin

--- funciones_aux.py
+++ funciones_aux.py
@@ -10,6 +10,7 @@ def val_int(valor):
             return entrada
         except Exception as e:
             mensajes_consola("input")
+            os.system("clear")
 
 def val_str(valor):
     while True:
@@ -17,49 +18,58 @@ def val_str(valor):
         try:
             entrada = int(valor)
             mensajes_consola("input")
+            os.system("clear")
         except Exception as e:
             return entrada
 
-def mensajes_consola(op):
+def mensajes_consola(op, extra=None):
     if op == "input":
         input("Opción incorrecta, ingrese un valor/opción correctas.\n[Enter - Continuar]\n>")
     elif op == "menu":
-        print("=" * 5 + " GESTOR DE CIUDADES - KRUSTY KRAB V0.1 " + "=" * 5 + "\n1 - Visilizar ciudades\n2 - Registrar ciudad\n3 - Modificar registro\n4 - Salir de Krusty Krab")
+        print("=" * 5 + " GESTOR DE CIUDADES - KRUSTY KRAB V0.1 " + "=" * 5 + "\n1 - Visualizar Ciudades\n2 - Registrar Ciudad\n3 - Modificar Registro\n4 - Salir de Krusty Krab")
     elif op == "ciudades":
         print("> PENDIENTE" + "\n1 - Visilizar ciudades\n2 - Registrar ciudad\n3 - Modificar registro\n4 - Salir de Krusty Krab")
+    elif op == "ver_registro":
+        ver_registro(extra)
 
-# def paginacion(structure_to_print, header, data_in_kwargs):
-#     start = 0
-#     pag_size = 5
-#     while True:
-#         end = start + pag_size
-#         print(header)
-#         if end > len(structure_to_print):
-#             end = len(structure_to_print)
-#         if start < 0:
-#             start = 0
-#         seccion = structure_to_print[start:end]
-#         current_page = ""
-#         for componente in seccion:
-#             line = ""
-#             if isinstance(structure_to_print,dict):
-#                 line = " | ".join(list(componente.values())) + "\n"
-#             else:
-#                 line = "".join(componente)
-#             current_page += line
-#         print(current_page)
-#         print(
-#             f"\t< {start} / {end} >\n[0 - Pagina anterior]    [1 - Pagina siguiente]\n[2 - Volver]")
-#         movimiento = int_val("> ", data_in_kwargs)
-#         if movimiento == 2:
-#             break
-#         elif movimiento == 1:
-#             start += pag_size
-#             if start > end:
-#                 start -= pag_size
-#         elif movimiento == 0:
-#             if start != 0:
-#                 start -= pag_size
+
+def ver_registro(registro):
+    keys = [key for key in registro[0]]
+    header = " | ".join(keys).upper() + "\n"
+    start = 0
+    pag_size = 5
+    while True:
+        os.system("clear")
+        print("> Visualizar Ciudades")
+        end = start + pag_size
+        print(header)
+        if end > len(registro):
+            end = len(registro)
+        if start < 0:
+            start = 0
+        seccion = registro[start:end]
+        current_page = ""
+        for ciudad in seccion:
+            line = ""
+            values = list(ciudad.values())
+            fixed_values = []
+            for i in values:
+                fixed_values.append(str(i()))
+            line = " | ".join(fixed_values) + "\n"
+            current_page += line
+        print(current_page)
+        print(
+            f"\t\t< {start} / {end} >\n[0 - Pagina anterior]    [1 - Pagina siguiente]\n[2 - Volver]")
+        movimiento = val_int(input("> "))
+        if movimiento == 2:
+            break
+        elif movimiento == 1:
+            start += pag_size
+            if start > end:
+                start -= pag_size
+        elif movimiento == 0:
+            if start != 0:
+                start -= pag_size
 
 
 
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import funciones_aux
+import datos
 
-funciones_aux.mensajes_consola("menu")
+ruta_json = "data.json"
+registro = datos.traer_datos(ruta_json)
+
+while True:
+    funciones_aux.os.system("clear")
+    funciones_aux.mensajes_consola("menu")
+    op = funciones_aux.val_int(input("> "))
+    if op < 1 or op > 4:
+        funciones_aux.mensajes_consola("input")
+    elif op == 1:
+        funciones_aux.mensajes_consola("ver_registro", registro)
